c74684019da4bb3215c700a1b738cf30e01124d3
MahmoudKSultan <mahmoodkns@hotmail.com>
2023-07-19 15:46:57 +0300

    adding setupTests.js + other changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Home, SigninPage,  SignupPage, NotFound } from "./pages";
 import { ProtectedRoute } from "./components";
 
 function App() {
   return (
-    <BrowserRouter>
     <Routes>
         <Route index element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="/sign-in" element={<SigninPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
         <Route path="*" element={<NotFound />} />
     </Routes>
-    </BrowserRouter>
   );
 }
 
